954abeb746fe6d79bfb6ac8788c4585927771d80
fly39244080 <fly39244080@163.com>
2019-08-30 10:52:04 +0800

    webpack end

---
+++ webpack/webpack-jqueryProjects 收官版/src/js/bank.js
@@ -0,0 +1,27 @@
+import 'styles/bank.less';
+// import $ from 'jquery';
+
+// import '../images/bank/ABC.jpg';
+import aa from 'images/bank/ABC.jpg';
+console.log(aa);
+console.log(config);
+console.log(sceneParam);
+
+console.log(process.env.NODE_ENV);
+
+// console.log($);
+// console.log(jQuery);
+
+$('#bankList').on('click',function(){
+    alert('bbb');
+})
+
+// var test = function(){
+//     return new Promise(function(resolve,reject){
+//         setTimeout(()=>{
+//             resolve('oo');
+//         },1000)
+//     })
+// }
+
+// test();
